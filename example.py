8f5ca0efa7af0dd4f30e21b8a7bcc311aeae747b
Lukasz Awsiukiewicz <114355811+awsiuk@users.noreply.github.com>
2025-03-28 17:44:04 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,16 @@
 from myENS160 import myENS160
+import machine
 
-obj=myENS160()
-TVOC=obj.getTVOC()
-AQI=obj.getAQI()
-ECO2=obj.getECO2()
+#I2C init for sensors readout
+#pin 11 & 12 is SDL  & SCA for ESP32-s3-nano
+SCL_PIN=machine.Pin(11)
+SDA_PIN=machine.Pin(12)
+i2c=machine.I2C(0,scl=SCL_PIN, sda=SDA_PIN,freq=400000)
+
+ens=myENS160(i2c)
+TVOC=ens.getTVOC()
+AQI=ens.getAQI()
+ECO2=ens.getECO2()
 print(AQI)
 print(TVOC)
 print(ECO2)
